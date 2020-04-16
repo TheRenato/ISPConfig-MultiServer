de519d75b8e4c86b559a962fd2cfa5d852b872f4
TheRenato <ropazo81@gmail.com>
2020-04-16 09:46:35 +0200

    Some modifications to the .env file. Install.sh hade Windows line breakes. added basic_functions.

---
+++ Scripts/basic_functions.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+# This script will collect all basic functions.
+#
+# Author: Renato Opazo Salgado
+# Date: 2020-04-15
+
+export $(egrep -v '^#' ./Scripts/.env | xargs)
+
+function ufw_nano_man
+{
+	if which ufw > /dev/null; then
+		echo "UFW Firewall is allready installed...		skipping"
+
+	else
+		apt -qy install ufw
+	fi
+	if which nano > /dev/null; then
+		echo "Nano editor is allready installed...		skipping"
+
+	else
+		apt -qy install nano vim-nox
+
+	fi
+	if which man > /dev/null; then
+		echo "Man manuall is allready installed...		skipping"
+
+	else
+		apt -qy install man
+	fi
+
+	echo "You have this versions in your server:"
+	echo ""
+	ufw version
+	nano -V
+	man -V
+}
+
+function add_hosts_to_hosts_file {
+  pattern="/The following/"
+  sed_i="i\\"
+  file_url="/etc/hosts"
+  sed -i "$pattern$sed_i$WEB_IP   $WEB_DOMAIN $WEB_HOSTNAME" $file_url
+  sed -i "$pattern$sed_i$MAIL_IP   $MAIL_DOMAIN $MAIL_HOSTNAME" $file_url
+  sed -i "$pattern$sed_i$DB_IP   $DB_DOMAIN $DB_HOSTNAME" $file_url
+  sed -i "$pattern$sed_i$NS1_IP   $NS1_DOMAIN $NS1_HOSTNAME" $file_url
+  sed -i "$pattern$sed_i$NS2_IP   $NS2_DOMAIN $NS2_HOSTNAME\n" $file_url
+}
+
+function change_hostname_file
+{
+  if [ -"$THIS_SERVER_IS" == "$WEB_HOSTNAME" ]; then
+    echo "$WEB_DOMAIN" > /etc/hostname
+
+  elif [ -"$THIS_SERVER_IS" == "$MAIL_HOSTNAME" ]; then
+    echo "$MAIL_HOSTNAME" > /etc/hostname
+    echo "$MAIL_DOMAIN" > /etc/mailname
+
+  elif [ -"$THIS_SERVER_IS" == "$DB_HOSTNAME" ]; then
+    echo "$DB_HOSTNAME" > /etc/hostname
+
+  elif [ -"$THIS_SERVER_IS" == "$NS1_HOSTNAME" ]; then
+    echo "$NS1_HOSTNAME" > /etc/hostname
+
+  elif [ -"$THIS_SERVER_IS" == "$NS2_HOSTNAME" ]; then
+    echo "$NS2_HOSTNAME" > /etc/hostname
+  fi
+
+  /etc/init.d/hostname.sh start
+
+}
+
+function create_swap {
+  cd /var
+  touch swap.img
+  chmod 600 swap.img
+  dd if=/dev/zero of=/var/swap.img bs=1024k count="$SWAP_CUSTOM_SIZE"
+  mkswap /var/swap.img
+  swapon /var/swap.img
+  echo "/var/swap.img    none    swap    sw    0    0" >> /etc/fstab
+}
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,32 +1,54 @@
 #! /bin/bash
 
 # This is a script with the main installation menu.
-# Is to prepare the server for ISPconfig
+# Is to prepare the server for ISPConfig
 #
 # Author: Renato Opazo Salgado
-# Date: 2020-04-14
+# Date 2020-04-15
+source ./Scripts/basic_functions.sh
+
+# This checks are from the servisys ispconfig_setup installation file
+if [[ "$#" -ne 0 ]]; then
+	echo -e "Usage: sudo $0" >&2
+	exit 1
+fi
+
+# Check if user is root
+if [[ $(id -u) -ne 0 ]]; then # $EUID
+	echo -e "Error: This script must be run as root, please run this script again with the root user or sudo." >&2
+	exit 1
+fi
+
+# Check if on Linux
+if ! echo "$OSTYPE" | grep -iq "linux"; then
+	echo -e "Error: This script must be run on Linux." >&2
+	exit 1
+fi
 
 echo "=========================================="
 echo ""
-echo "So, you want to prepare your server for ISPconfig."
+echo "So, you want to prepare your server for ISPConfig"
 echo ""
-echo "v1.00"
+echo "v1.01"
 echo "=========================================="
 echo ""
-echo "1)	Update and Upgrade the Server."
-echo "2)	Install ufw, nano, vim-nox and Expect. And the system will reboot"
-echo "3)	Enable ufw and allow SSH traffic."
+echo "1)	First run Update and Upgrade the Server."
+echo "  	And check if needed to install ufw, nano, vim-nox."
+echo "2)	Enable ufw and allow only SSH traffic."
 echo " "
+echo "3)	Have you edited the .env file? No?"
+echo "  	Then lets do it now. We will use Nano"
 echo " "
-echo "4)	Install Apache2 and allow HTTP and HTTPS traffic."
-echo "		MariaDB and PHP will be also installed."
-echo "5)	Install WordPress."
+echo "4)	Lets add your .env file data to the hosts and hostname file."
+echo " "
+echo "5)  Now we will create the swap file. "
+echo "    OBSERVE! This will NOT check if you have SWAPON or not."
 echo " "
 echo "6)	Install Nginx and allow HTTP and HTTPS traffic."
 echo "7)	Add a Nginx webpage in external server with load balancer."
 echo "8)	Add a Nginx webpage in this server"
 echo " 		"
-echo "9)	Install Node.js and template."
+echo "9)	Reboot"
 echo "	"
 echo "[CTRL] + [C])	Just Exit"
 echo ""
@@ -36,11 +58,17 @@ read varNr
 
 if [ "$varNr" -eq 1 ]
 then
-	sudo apt update
-	sudo apt -qy upgrade
+	apt update
+	apt -qy upgrade
+	ufw_nano_man
+	apt-get -y install ntp ntpdate
 	echo " "
 	echo " "
 	echo "Done!! "
+	echo " "
+	echo " "
+	echo " "
+	echo "You should reboot "
 	echo " "
 	echo " "
 	./Scripts/install.sh
@@ -48,9 +76,8 @@ then
 
 elif [ "$varNr" -eq 2 ]
 then
-	ufw_nano_man
-	sudo apt-get -yq install expect
-	sudo apt -yq install pwgen
+	ufw allow ssh
+	ufw enable
 	echo " "
 	echo " "
 	echo "Done!! "
@@ -59,17 +86,11 @@ then
 	echo " "
 	echo " "
 	echo " "
-	echo "Sorry must reboot "
-	echo " "
-	echo " "
-	echo " "
-	echo " "
-	sudo reboot
+	./Scripts/install.sh
 
 elif [ "$varNr" -eq 3 ]
 then
-	sudo ufw allow ssh
-	sudo ufw enable
+	nano ./Scripts/.env
 	echo " "
 	echo " "
 	echo "Done!! "
@@ -79,7 +100,8 @@ then
 
 elif [ "$varNr" -eq 4 ]
 then
-	apache_php_inst
+  add_hosts_to_hosts_file
+  change_hostname_file
 	echo " "
 	echo " "
 	echo "Done!! "
@@ -89,7 +111,7 @@ then
 
 elif [ "$varNr" -eq 5 ]
 then
-	wp_inst
+	create_swap
 	echo " "
 	echo " "
 	echo "Done!! "
@@ -127,12 +149,14 @@ then
 
 elif [ "$varNr" -eq 9 ]
 then
-	nodejs_inst
 	echo " "
 	echo " "
-	echo "Done!! "
+	echo "We will reboot!! "
+	echo "To start the script again write: "
+	echo "./Scripts/install.sh"
 	echo " "
 	echo " "
+	reboot
 
 
 else
